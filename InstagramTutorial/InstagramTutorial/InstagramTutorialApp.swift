8c1960c9848f45769fe89e138704944c4c22b368
timotheekim10 <timotheekim10@gmail.com>
2023-11-02 13:20:36 +0900

    add: 인스타그램 클론 코딩 프로젝트 추가

---
+++ InstagramTutorial/InstagramTutorial/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by 티모시 킴 on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
